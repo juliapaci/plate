e58b8b712da2948bc551bf8015db0c816fb921c4
julia <fabianpaci@protonmail.com>
2024-07-10 19:36:54 +1000

    compile commands

--- nob.c
+++ nob.c
@@ -2,14 +2,19 @@
 #include "nob.h"
 
 #define BUILD "build"
-#ifdef DEBUG
-#define CFLAGS "-Wall", "-Wextra", "-ggdb"
-#else
 #define CFLAGS "-Wall", "-Wextra"
-#endif // DEBUG
+// TODO: not sure if "-I" is an linker flag?
+#define LDFLAGS "-Lbuild", "-Isrc/protocol", "-lprotocol"
+// TODO: find a way to automate making this
+#define LDFLAGS_STRING "\"-Lbuild\", \"-Isrc/protocol\", \"-lprotocol\""
+
+void build_protocol(void) {
+    CMD("cc", CFLAGS, LDFLAGS, "-c", "-o", PATH(BUILD, "protocol.o"), PATH("src", "protocol", "protocol.c"));
+    CMD("ar", "-rcs", PATH(BUILD, "libprotocol.a"), PATH(BUILD, "protocol.o"));
+}
 
 void build_server(void) {
-    // CMD("mkdir", "-p", "server");
+    // CMD("mkdir", "-p", PATH(BUILD, "server"));
 
     // TODO: real incremental compilation
     // FOREACH_FILE_IN_DIR(src, PATH("src", "server"), {
@@ -20,19 +25,41 @@ void build_server(void) {
     //             "-c", src
     //         );
     // });
-    CMD("cc", CFLAGS, "-o", PATH(BUILD, "server"), PATH("src", "server", "main.c"), PATH("src", "server", "server.c"));
+    CMD("cc", CFLAGS, LDFLAGS, "-o", PATH(BUILD, "server"), PATH("src", "server", "main.c"), PATH("src", "server", "server.c"));
 }
 
 void build_client(void) {
-    CMD("cc", CFLAGS, "-o", PATH(BUILD, "client"), PATH("src", "client", "main.c"), PATH("src", "client", "client.c"));
+    CMD("cc", CFLAGS, LDFLAGS, "-o", PATH(BUILD, "client"), PATH("src", "client", "main.c"), PATH("src", "client", "client.c"));
+}
+
+// NOTE: very bare bones just so clangd can pick up on stuff
+void create_compile_commands(void) {
+    CMD("touch", "compile_commands.json");
+
+    FILE *json = fopen("compile_commands.json", "w");
+    fprintf(json,
+        "["
+        "{"
+        "\"directory\": \"%s\","
+        "\"arguments\": [/usr/bin/cc, %s],"
+        "\"file\": N/A"
+        "}"
+        "]",
+        GETCWD(),
+        LDFLAGS_STRING
+    );
+
+    fclose(json);
 }
 
 int main(int argc, char **argv) {
     GO_REBUILD_URSELF(argc, argv);
 
     CMD("mkdir", "-p", BUILD);
-    build_server();
-    build_client();
+    INFO("building protocol"); build_protocol();
+    INFO("building server"); build_server();
+    INFO("building client"); build_client();
+    INFO("creating \"compile_commands.json\""); create_compile_commands();
 
     return 0;
 }
