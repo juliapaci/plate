33a40f6c02adfdbec9ae7feb4f23e08e59fd2ae4
julia <fabianpaci@protonmail.com>
2024-07-11 21:52:33 +1000

    compile_commands automated

--- nob.c
+++ nob.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #define NOBUILD_IMPLEMENTATION
 #include "nob.h"
 
@@ -5,12 +6,10 @@
 #define CFLAGS "-Wall", "-Wextra"
 // TODO: not sure if "-I" is an linker flag?
 #define LDFLAGS "-Lbuild", "-Isrc/protocol", "-lprotocol"
-// TODO: find a way to automate making this
-#define LDFLAGS_STRING "\"-Lbuild\", \"-Isrc/protocol\", \"-lprotocol\""
 
 void build_protocol(void) {
     CMD("cc", CFLAGS, LDFLAGS, "-c", "-o", PATH(BUILD, "protocol.o"), PATH("src", "protocol", "protocol.c"));
-    CMD("ar", "-rcs", PATH(BUILD, "libprotocol.a"), PATH(BUILD, "protocol.o"));
+    CMD("ar", "rcs", PATH(BUILD, "libprotocol.a"), PATH(BUILD, "protocol.o"));
 }
 
 void build_server(void) {
@@ -25,30 +24,44 @@ void build_server(void) {
     //             "-c", src
     //         );
     // });
-    CMD("cc", CFLAGS, LDFLAGS, "-o", PATH(BUILD, "server"), PATH("src", "server", "main.c"), PATH("src", "server", "server.c"));
+    CMD("cc", CFLAGS,  "-o", PATH(BUILD, "server"), PATH("src", "server", "main.c"), PATH("src", "server", "server.c"), LDFLAGS);
 }
 
 void build_client(void) {
-    CMD("cc", CFLAGS, LDFLAGS, "-o", PATH(BUILD, "client"), PATH("src", "client", "main.c"), PATH("src", "client", "client.c"));
+    CMD("cc", CFLAGS, "-o", PATH(BUILD, "client"), PATH("src", "client", "main.c"), PATH("src", "client", "client.c"), LDFLAGS);
 }
 
 // NOTE: very bare bones just so clangd can pick up on stuff
 void create_compile_commands(void) {
     CMD("touch", "compile_commands.json");
-
     FILE *json = fopen("compile_commands.json", "w");
+
+    const char *ldflags_array[] = {LDFLAGS};
+    const size_t ldflags_amount = sizeof(ldflags_array)/sizeof(char *);
+    size_t ldflags_size = 0;
+    for(size_t i = 0; i < ldflags_amount; i++)
+        ldflags_size += strlen(ldflags_array[i]) + strlen("\", \"");
+
+    char *ldflags_args = malloc(ldflags_size);
+    ldflags_args[0] = '\0';
+    for(size_t i = 0; i < sizeof(ldflags_array)/sizeof(char *); i++) {
+        ldflags_args = strcat(ldflags_args, "\", \"");
+        ldflags_args = strcat(ldflags_args, ldflags_array[i]);
+    }
+
     fprintf(json,
         "["
         "{"
         "\"directory\": \"%s\","
-        "\"arguments\": [/usr/bin/cc, %s],"
-        "\"file\": N/A"
+        "\"arguments\": [\"/usr/bin/cc%s\"],"
+        "\"file\": \"N/A\""
         "}"
         "]",
         GETCWD(),
-        LDFLAGS_STRING
+        ldflags_args
     );
 
+    free(ldflags_args);
     fclose(json);
 }
 
